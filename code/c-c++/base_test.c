f0205a1723456979c6c50b185cecb3d39076ff6c
root <root@localhost.localdomain>
2015-08-28 17:51:01 +0800

    update

--- code/c-c++/base_test.c
+++ code/c-c++/base_test.c
@@ -3,6 +3,15 @@ typedef void *QUEUE[2];
 
 #define QUEUE_NEXT(q)	(*(QUEUE **) &((*(q))[0]))
 #define QUEUE_PREV(q)	(*(QUEUE **) &((*(q))[1]))
+
+void fnThree()
+{
+	int a = 3;
+	int b = 1;
+	(a > b)?(a=2):(a=0);
+	printf("a = %d\n", a);
+}
+
 int main()
 {
 	int n = 1;
@@ -12,6 +21,11 @@ int main()
 	printf("%x, %x, %x\n", q, &(q[0]), &(q[1]));
 	printf("%x, %x, %x\n", q, &QUEUE_NEXT(&q), &QUEUE_PREV(&q));
 	printf("%x, %x, %x\n", q, &((*(q))[0]), &((*(q))[1]) );
+
+	printf("test fnThree...");
+	fnThree();
+	printf("Test : %.1f\n", 0.382525);
+	
 	getchar();
 	return 0;
 }
