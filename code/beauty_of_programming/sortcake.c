1f98c493a9d2c41c332108772e49970731340a13
pk <pkxpp@126.com>
2016-02-18 07:00:44 -0800

    quick sort

--- code/beauty_of_programming/sortcake.c
+++ code/beauty_of_programming/sortcake.c
@@ -26,6 +26,7 @@ void quick_sort(int a[], int start, int end)
 {
 	int i = 0;	// for circle
 	int pivot = start;
+	int nFind = 0;
 	if (start >= end)
 		return;
 	for(i = start+1; i < end; i++)
@@ -33,14 +34,46 @@ void quick_sort(int a[], int start, int end)
 		if (a[i] < a[pivot])
 		{
 			//swap(a+i, a+pivot);
-			swap(&a[i], &a[pivot]);
-			pivot = i;
+			//swap(&a[i], &a[pivot]);
+			nFind = 1;
+			pivot++;
 		}	
+		
+		if (nFind == 1 && i != pivot)
+			swap(&a[i], &a[pivot]);
+		nFind = 0;
 	}
+	
+	if (pivot != start)
+		swap(&a[pivot], &a[start]);
 	print(a, end);
 	// recursive
-	//quick_sort(a, start, pivot);
-	//quick_sort(a, pivot+1, end);
+	quick_sort(a, start, pivot);
+	quick_sort(a, pivot+1, end);
+}
+
+int partition(int a[], int start, int end)
+{
+	int pivot = a[start];
+	while( start < end){
+		while(a[end] >= pivot && start < end) end--;
+		a[start] = a[end];
+		while(a[start] <= pivot && start < end) start++;
+		a[end] = a[start];
+	}
+	a[start] = pivot;
+
+	return start;
+}
+
+void quick_sort2(int a[], int start, int end)
+{
+	if (start < end)
+	{
+		int pivot = partition(a, start, end);
+		quick_sort2(a, start, pivot-1);
+		quick_sort2(a, pivot+1, end);
+	}
 }
 
 /* main */
@@ -51,7 +84,7 @@ int main()
 	printf("sort befaore: \n");
 	print(a, len);
 
-	quick_sort(a, 0, len);
+	quick_sort2(a, 0, len-1);
 	printf("sort after: \n");
 	print(a, len);
 
