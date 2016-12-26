62a91f5a8894af346fe64a63a482a6e13fbdff8b
page <pkxpp@126.com>
2016-12-26 13:15:05 +0800

    :rocket: struture alignment

---
+++ code/c-c++/structure_alignment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+ 
+#define PRINT_D(intValue)     printf(#intValue" is %d\n", (intValue));
+#define OFFSET(struct,member)  ((char *)&((struct *)0)->member - (char *)0)
+ 
+typedef struct
+{
+    char    sex;
+    short   score;
+    //char    a;		// add this member, sizeof is 12; and offset is 4
+    int     age;
+}student;
+ 
+int main()
+{
+    PRINT_D(sizeof(student))
+    PRINT_D(OFFSET(student,sex))
+    PRINT_D(OFFSET(student,score))
+    //PRINT_D(OFFSET(student,a))
+    PRINT_D(OFFSET(student,age))
+    return 0;
+}
