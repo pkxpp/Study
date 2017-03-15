1eed84e92a93ced2f4f602e47b39eff80d8a9e7e
pkxpp <pkxpp@126.com>
2017-03-15 16:57:20 +0800

    big-little endian judge

---
+++ code/visualstudio/vs/Projects/Study/fragment/fragment.cpp
@@ -0,0 +1,16 @@
+// fragment.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// 1. check system endian
+	check_endian();
+
+
+	getchar();
+	return 0;
+}
+
