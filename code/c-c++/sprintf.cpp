6a9af725b3096cd6f0fc0263804864476883b89c
pkxpp <pkxpp@126.com>
2017-07-06 08:17:20 +0800

    snprintf return value

--- code/c-c++/sprintf.cpp
+++ code/c-c++/sprintf.cpp
@@ -4,13 +4,14 @@
 int main()
 {
     char buf[20];
-    char *pstr = "this is a test string";
+    char *pstr = "this is a test string aaaaaaaaaaaa";
 
     memset(buf, 0, sizeof(buf));
     // windows use _snprintf function.
-    snprintf(buf, sizeof(buf)-1, "%s", pstr);
+    // Notice: return value!
+    int len = snprintf(buf, sizeof(buf)-1, "%s", pstr);
 
-    printf("buf = %s, strlen(buf) = %d\n", buf, strlen(buf));
+    printf("buf = %s, strlen(buf) = %d, len = %d\n", buf, strlen(buf), len);
 
     return 0;
 
