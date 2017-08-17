7d7194dea9511910f4dc6af915cd1509af7f29fb
pkxpp <pkxpp@126.com>
2017-08-17 11:09:36 +0800

    function template must be careful

---
+++ code/visualstudio/vs/Projects/Study/cpp2lua/main.cpp
@@ -0,0 +1,136 @@
+#include "Test.hpp"
+
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+USING_NAMESPACE_LUAREG
+
+// eg: 1
+template<class T> void f( T);				// (b) 基础模板
+template<class T> void f( int, T,double );	// (c) 基础模板重载
+template<class T> void f( T*);				// (d) 基础模板重载
+template<> void f<int>( int);				// (e) 基础模板(b)特化
+void f( double);							// (f) 非模板函数，重载
+
+
+// eg: 2
+template<class T>	 // (a2) 一个基础模版
+void func( T );
+
+template<class T>	 // (b2) 另一个基础模版，(a2)的重载版本
+void func( T* );		 //     （函数模版没有偏特化；可以用重载替代）
+
+template<>			 // (c2) (b2)模版的全特化
+void func<>(int*);
+
+// eg: 3
+// (a3) 和之前一样的基础模版
+template<class T> 
+void f3( T )
+{
+	cout << "a3 function" << endl;
+}
+// (c3) 针对(a3)的完全特化
+template<>       
+void f3<>(int*)
+{
+	cout << "c3 function" << endl;
+}
+// (b3) 另一个基础模版，对(a3)的重载
+template<class T> 
+void f3( T* )
+{
+	cout << "b3 function" << endl;
+}
+
+void LuaTest() {
+  REGISTER_LUA_CALSS(Test);
+  
+  Test* test = new Test();
+  
+  test->m_data = 1001;
+  test->m_data2 = 1002;
+  g_luaReg->RegisterObject("test", test);
+  g_luaReg->DoScript("Test.lua");
+}
+
+int main() {
+  LuaInit();
+  LuaTest();
+  LuaClear();
+
+  // test 1
+  bool b = true;
+  int i = 1;
+  double d = 1.1f;
+  f( b);        // 调用(b)模版 模版参数T = bool
+  f( i, 42, d ); // 调用(c)模版 T = int
+  f( &i);       // 调用(d)模版 T = int
+  f( i);        // 调用(e)模版
+  f( d);        // 调用(f)函数
+
+  // test 2
+  int n = 2;
+  int *p = &n;
+  func( p);           // 调用(c2)
+
+  // test3
+  f3(p);
+
+  getchar();
+  return 0;
+}
+ 
+// (b) 基础模板
+template<class T> void f( T)
+{
+	cout << "(b)" << endl;
+}
+
+// (c) 基础模板重载
+template<class T> void f( int, T,double )
+{
+	cout << "(c)" << endl;
+}
+
+// (d) 基础模板重载
+template<class T> void f( T*)
+{
+	cout << "(d)" << endl;
+}
+
+// (e) 基础模板(b)特化
+template<> void f<int>( int)
+{
+	cout << "(e)" << endl;
+}
+
+// (f) 非模板函数，重载
+void f( double)
+{
+	cout << "(f)" << endl;
+}
+
+//////////////////////////////////////////////////////////////////////////
+ // (a2) 一个基础模版
+template<class T>	
+void func( T )
+{
+	cout << "a2 func" << endl;
+}
+
+// (b2) 另一个基础模版，(a2)的重载版本（函数模版没有偏特化；可以用重载替代）
+template<class T>
+void func( T* )
+{
+	cout << "b2 func" << endl;
+}
+
+// (c2) (b2)模版的全特化
+template<>
+void func<>(int*)
+{
+	cout << "c2 func" << endl;
+}
---
+++ code/visualstudio/vs/Projects/Study/stackoverflow/stackoverflow.cpp
@@ -0,0 +1,150 @@
+// stackoverflow.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "BinaryTree.h"
+#include<vector>
+#include<algorithm>
+#include<cstdlib>
+#include<numeric>
+#include<ctime>
+#include <stdio.h>
+#include "sort.h"
+#include <stdlib.h>
+#include <sstream>
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////
+// 1. seg fault
+// 2. Binary Tree
+//////////////////////////////////////////////////////////////////////////
+// seg fault
+
+void test1(){
+	char* commandLine = "use me 123456 abc";
+	int state = 0;
+	char *token = strtok(commandLine, " ");
+	string c, u, p, a;
+	while(token != NULL)
+	{
+		switch(state)
+		{
+		case 0: c = token; break;
+		case 1: u = token; break;                          
+		case 2: p = token;break;                        
+		case 3: a = token; break;            
+		}
+		token = std::strtok(NULL, " ");
+		state++;
+	}
+
+	const char* command = c.c_str();
+	const char* user = u.c_str();
+	const char* password = p.c_str();
+	const char* args = a.c_str();
+};
+
+//////////////////////////////////////////////////////////////////////////
+// 4.Binary Tree
+void TestBinaryTree()
+{
+	int randNum;
+	vector<int> randData;
+	vector<int>::iterator iter;
+	size_t size = randData.size();
+	srand(time(0));
+	BinaryTree tree;
+	cout << "number: ";
+	for (int i = 0; i < 5; i++)
+	{
+		randNum = rand() % 1000 + 1;
+		tree.insertNode(randNum);
+		
+		cout << randNum << " ";
+	}
+	cout << endl << "display : \n";
+	tree.displayInOrder();
+	cout << endl;
+	for(int i=1; i<=5;i++)
+	{ 
+		cout<< "number: " << i << " level = "<< tree.getLevel(i) << endl;
+	}
+}
+//////////////////////////////////////////////////////////////////////////
+void TestQuickSort()
+{
+
+	// The user inputs a string of numbers (e.g. "6 4 -2 88 ..etc") and those integers are then put into a vector named 'vec'.
+	std::vector<int> vec;
+
+	// Converts string from input into integer values, and then pushes said values into vector.
+	std::string line;
+	if ( getline(std::cin, line) )
+	{
+		std::istringstream str(line);
+
+		int value;
+		str >> value;
+		vec.push_back( value );
+		while ( str >> value ) 
+		{
+			vec.push_back( value );
+		}
+	}
+	// Creating QuickSort object.
+	QuickSort qSort;
+	QuickSort *ptrQSort = &qSort;
+	// Creating new vector that has been 'Quick Sorted'.
+	int vecSize = vec.size();
+	std::vector<int> qSortedVec;
+	qSortedVec = ptrQSort->sortFunc( vec, 0, vecSize-1 );
+
+	// Middle, start, and end positions on the vector.
+	int mid = ( 0 + (vec.size()-1) )  / 2;
+	int start = 0, end = vec.size() - 1;
+
+	// Creating RecursiveBinarySearch object.
+	RecursiveBinarySearch bSearch;
+	RecursiveBinarySearch *ptrBSearch = &bSearch;
+	//bool bS = ptrBSearch->binarySearch( qSortedVec, mid, start, end );
+	bool bS = ptrBSearch->binarySearch( bSortedVec, mid, start, end );
+
+
+	/*--------------------------------------OUTPUT-----------------------------------------------------------------------*/
+
+	// Print out inputted integers and the binary search result.
+
+	// Depending on the binary search, print either 'true' or 'false'.
+	if ( bS == 1 )
+	{
+		std::cout << "true ";
+	}
+	if ( bS == 0 )
+	{
+		std::cout << "false ";
+	}
+
+	// Prints the result of the 'quick sorted' array.
+	int sortedSize = qSortedVec.size();
+	for ( int i = 0; i < sortedSize; i++ )
+	{
+		std::cout << qSortedVec[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+//////////////////////////////////////////////////////////////////////////
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// 1.seg fault
+	//test1();
+
+	// 2. Binary Tree
+	TestBinaryTree();
+
+	getchar();
+	return 0;
+}
+
