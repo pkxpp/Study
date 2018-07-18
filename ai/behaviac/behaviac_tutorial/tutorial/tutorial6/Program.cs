8ae4adbf7dcdc28aac2ef079806f6c84bdc23af6
PeiKai [裴凯] <PeiKai@kingsoft.cn>
2018-07-18 09:23:22 +0800

    behaviac study

---
+++ ai/behaviac/behaviac_tutorial/tutorial/tutorial6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial4
+{
+    class Program
+    {
+        static FirstAgent g_FirstAgent;
+        const string strBehaviorTree = "maintree_task";//"LoopBT";
+
+        static bool InitBehavic()
+        {
+            Console.WriteLine("InitBehavic");
+
+            behaviac.Workspace.Instance.FilePath = "../../exported";
+            behaviac.Workspace.Instance.FileFormat = behaviac.Workspace.EFileFormat.EFF_xml;
+
+            return true;
+        }
+
+        static bool InitPlayer()
+        {
+            Console.WriteLine("InitPlayer");
+
+            g_FirstAgent = new FirstAgent();
+
+            bool bRet = g_FirstAgent.btload(strBehaviorTree);
+            //Debug.Assert(bRet);
+
+            g_FirstAgent.btsetcurrent(strBehaviorTree);
+
+            return bRet;
+        }
+
+        static void UpdateLoop()
+        {
+            Console.WriteLine("UpdateLoop");
+
+            int frames = 0;
+            behaviac.EBTStatus status = behaviac.EBTStatus.BT_RUNNING;
+
+            while (status == behaviac.EBTStatus.BT_RUNNING)
+            {
+                Console.WriteLine("frame {0}", ++frames);
+
+                status = g_FirstAgent.btexec();
+
+                g_FirstAgent.FireEvent("event_task", 2);
+            }
+        }
+
+        static void CleanupPlayer()
+        {
+            Console.WriteLine("CleanupPlayer");
+
+            g_FirstAgent = null;
+        }
+
+        static void CleanupBehaviac()
+        {
+            Console.WriteLine("CleanupBehaviac");
+
+            behaviac.Workspace.Instance.Cleanup();
+        }
+
+        static void Main(string[] args)
+        {
+            InitBehavic();
+
+            InitPlayer();
+
+            UpdateLoop();
+
+            CleanupPlayer();
+
+            CleanupBehaviac();
+
+            Console.Read();
+        }
+    }
+}
